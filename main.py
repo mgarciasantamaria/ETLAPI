b5b37ee91733cd4d1674e8572f791d7589407b83
Mario Garcia <mandres_garcia@hotmail.com>
2023-09-08 17:04:27 -0500

    pass

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from routers import toolboxapi
+from routers import toolboxapi, testapi
 
 app = FastAPI()
 
+app.include_router(testapi.apitest)
 app.include_router(toolboxapi.toolboxapp)
 
 #$ uvicorn main:app --port 5000 --ssl-keyfile=./key.pem --ssl-certfile=./cert.pem
