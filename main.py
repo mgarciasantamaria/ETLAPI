403589af3dc948d12812e8da7b5eb952991d3c3b
Mario Garcia <mandres_garcia@hotmail.com>
2023-09-08 16:59:09 -0500

    pass

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from routers import vtrapi, toolboxapi
+from routers import toolboxapi
 
 app = FastAPI()
 
-app.include_router(vtrapi.vtrapp)
 app.include_router(toolboxapi.toolboxapp)
 
 #$ uvicorn main:app --port 5000 --ssl-keyfile=./key.pem --ssl-certfile=./cert.pem
